3919bc3cf81263119f9ba1be8f1653f1686e7951
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-14 02:53:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,12 +46,17 @@ def total_gross(source)
   collective_total = 0
   dir_arr = list_of_directors(source)
   dir_hash = directors_totals(source)
-  # i = 0 
-  # while i < dir_arr.length do 
-  #   dir_name = dir_arr[i]
+  
+  
+  
+  
+  
+  i = 0 
+  while i < dir_arr.length do 
+    dir_name = dir_arr[i]
     
-  # i+=1 
-  # end
+  i+=1 
+  end
   puts dir_hash
   # collective_total
 end
