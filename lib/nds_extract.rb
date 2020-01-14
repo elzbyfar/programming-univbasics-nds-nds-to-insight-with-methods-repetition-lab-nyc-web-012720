e4a260a9d4395660e2a1a3d7864dbf4d7313350c
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-14 02:02:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,7 +49,7 @@ def total_gross(source)
   #   collective_total += directors_totals(source)
   # i+=1 
   # end
-  puts directors_totals(source[0][:name])
+  puts source.class
   # collective_total
 end
 
